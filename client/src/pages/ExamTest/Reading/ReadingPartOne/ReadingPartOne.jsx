25239ea82a39d1c8483bee0e1980751c2ad0d6a6
PhamDuong113 <129020048+PhamDuong113@users.noreply.github.com>
2024-11-06 21:21:51 +0700

    feat: update ReadingPartOne component to render dynamic sub-questions and improve content handling

--- client/src/pages/ExamTest/Reading/ReadingPartOne/ReadingPartOne.jsx
+++ client/src/pages/ExamTest/Reading/ReadingPartOne/ReadingPartOne.jsx
@@ -1,100 +1,84 @@
-import { Box, Button, Typography } from '@mui/material';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../Reading/ExamReading.css';
 import { useSelector } from 'react-redux';
 import { RES_DATA } from '../../../../Constant/global';
+
+const TITLE = 0;
+const DEAR_PERSON = 1;
+const FOOT_FISH = 2;
+const SIGNAL = 3;
 
 const ReadingPartOne = () => {
 	const testBankData = useSelector(
 		(state) => state.testBankStore.testBankData
 	);
 
-	const [resReadingPartOne, setResReadingPartOne] = React.useState();
-	const [contentPartOne, setContentPartOne] = React.useState();
+	const [resReadingPartOne, setResReadingPartOne] = useState();
+	const [contentPartOne, setContentPartOne] = useState();
 
 	useEffect(() => {
-		console.log({ testBankData });
-
 		const readingPartOne = testBankData.reading.part1[RES_DATA].data;
 		setResReadingPartOne(readingPartOne);
+		setContentPartOne(readingPartOne?.questions);
+	}, [testBankData]);
 
-		setContentPartOne(readingPartOne?.questions.subQuestion);
-	}, []);
-	return (
-		<>
-			<div>
-				<div className="lrn_stimulus_content lrn_clearfix lrn_question mb-5">
-					<b>Read the email from Janice to her friend.&nbsp;</b>
-					<strong>
-						Choose one word from the list for each gap. The first one is
-						done for you.
-					</strong>
-					{/* {resReadingPartOne?.questions.content} */}
-				</div>
-
-				<div>
-					<div class=" lrn_response_innerbody lrn-response-validate-wrapper">
-						<div class=" lrn_response_input">
-							<div class="">
-								<p>Dear Sally,</p>
-
-								<div className="flex justify-start flex-col gap-6">
-									{contentPartOne?.map((item, index) => {
-										return (
-											<>
-												<div className="flex justify-start gap-1 text-sm">
-													{item.content
-														.split(' ')
-														.map((word, index) => {
-															return word !== 'tentisspace' ? (
-																<div>{word}</div>
-															) : (
-																<span data-lrn-template-response="">
-																	<span className="lrn_combodiv">
-																		<select
-																			aria-label="Response input area"
-																			className="lrn-cloze-select lrn_cloze_response "
-																			data-inputid="1"
-																		>
-																			<option
-																				role="option"
-																				value=""
-																				aria-label="Please select an option - "
-																			></option>
-
-																			{item.answerList.length > 0 &&
-																				item.answerList.map(
-																					(answer, index) => {
-																						return (
-																							<option
-																								role="option"
-																								value={answer.content}
-																							>
-																								{answer.content}
-																							</option>
-																						);
-																					}
-																				)}
-																		</select>
-																	</span>
-																</span>
-															);
-														})}
-												</div>
-											</>
-										);
-									})}
-								</div>
-
-								<p>Love,</p>
-
-								<p>Janice</p>
-							</div>
-						</div>
-					</div>
-				</div>
+	const renderSubQuestions = (subQuestions) => {
+		return subQuestions.map((item, index) => (
+			<div key={index} className="flex justify-start gap-1 text-sm">
+				{item.content.split(' ').map((word, idx) =>
+					word !== 'tentisspace' ? (
+						<div key={idx}>{word}</div>
+					) : (
+						<span key={idx} data-lrn-template-response="">
+							<span className="lrn_combodiv">
+								<select
+									aria-label="Response input area"
+									className="lrn-cloze-select lrn_cloze_response"
+									data-inputid="1"
+								>
+									<option
+										role="option"
+										value=""
+										aria-label="Please select an option - "
+									></option>
+									{item.answerList.map((answer, idx) => (
+										<option
+											key={idx}
+											role="option"
+											value={answer.content}
+										>
+											{answer.content}
+										</option>
+									))}
+								</select>
+							</span>
+						</span>
+					)
+				)}
 			</div>
-		</>
+		));
+	};
+
+	return (
+		<div>
+			<div className="lrn_stimulus_content lrn_clearfix lrn_question mb-5">
+				<b>{contentPartOne?.content.split('tentisspace')[TITLE]}</b>
+			</div>
+			<div className="lrn_response_innerbody lrn-response-validate-wrapper">
+				<div className="lrn_response_input"></div>
+				<p>
+					{contentPartOne?.content.split('tentisspace')[DEAR_PERSON]}
+				</p>
+				<div className="flex justify-start flex-col gap-6">
+					{contentPartOne?.subQuestion &&
+						renderSubQuestions(contentPartOne.subQuestion)}
+				</div>
+				<div>
+					{contentPartOne?.content.split('tentisspace')[FOOT_FISH]}
+				</div>
+				<p>{contentPartOne?.content.split('tentisspace')[SIGNAL]}</p>
+			</div>
+		</div>
 	);
 };
 
