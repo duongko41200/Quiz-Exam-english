c76adab0aa13229174c2f65fafa5343c733b0ad1
duongMyColor <165616663+duongMyColor@users.noreply.github.com>
2025-01-14 16:17:29 +0700

    feat: enhance error handling in ErrorBoundary with automatic redirection to home page

--- client/src/middleware/ErrorBoundary.jsx
+++ client/src/middleware/ErrorBoundary.jsx
@@ -1,14 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 import "react-toastify/dist/ReactToastify.css";
 
 // Custom Fallback Component
 export function ErrorFallback({ error, resetErrorBoundary }) {
+      const navigate = useNavigate();
+
+      // Chuyển hướng về trang chủ khi có lỗi
+      React.useEffect(() => {
+        // Sau khi bắt lỗi, điều hướng về trang chủ (hoặc bất kỳ trang nào)
+        navigate("/");
+      }, [error, navigate]);
   return (
     <div role="alert">
-      <p>Something went wrong:</p>
-      <pre>{error.message}</pre>
-      <button onClick={resetErrorBoundary}>Try again</button>
+      <p>Something went wrong: {error.message}</p>
+      <p>Redirecting to the home page...</p>
     </div>
   );
 }
